20e696444b34c9bcf125de346c9328fc19a649ff
M. Valentina Castro <valentinacastro1516@gmail.com>
2021-11-08 09:30:20 -0500

    Read Input and Write Output completed

--- src/ui/HanoiSystem.java
+++ src/ui/HanoiSystem.java
@@ -1,22 +1,37 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class HanoiSystem {
 
 	private static int [] towers; 
 	
-	public static void main(String[] args) 
-	{
+	private static BufferedReader br;	
+	private static BufferedWriter bw;
+	private static final String TEST_FILE = "data/test";
+	private static final String TEST_OUTPUT_FILE = "data/testOutput.txt";
+
+	public static void main(String[] args) throws IOException 
+	{	
 		towers = new int [3];
+		//towers[0] = 3;
+		
+		br = new BufferedReader(new FileReader(TEST_FILE) );
+		bw = new BufferedWriter(new FileWriter(TEST_OUTPUT_FILE));
 		
 		
-		towers[0] = 3;
+		importData();
 		
-		printMovements();
-		move(towers[0], 0, 1, 2);
+		//printMovements();
+		//move(towers[0], 0, 1, 2);
 		
 	}
 	
-	/* Si disks (n) = 0 -> NO  SE HACE NADA
+	/*  Si disks (n) = 0 -> NO  SE HACE NADA
 	 *  Si disks = 1 -> Pasar de A ([0]) a C ([2])
 	 *  Si disks > 1:
 	 *  	 -> Mover disks-1 a B ([1])  
@@ -24,36 +39,71 @@ public class HanoiSystem {
 	 *		 -> Mover disk-1 (que sería como otra torre de tamaño disks-1) a C ([2]) encima del disk restante.
 	 */
 	
-	
-	public static void move( int disks, int towerA, int towerB, int towerC )
+	public static void move( int disks, int towerA, int towerB, int towerC ) throws IOException
 	{
-		//System.out.println("Llamo el metodo");
-		//System.out.println(disks);
+		
 		if(disks == 1)
 		{
 			towers[towerA] = towers[towerA]-1; 
 			towers[towerC] = towers[towerC]+1;
-			printMovements();
+			//printMovements();
+			bw.write( printMovements());
 		}
-		else //if( disks > 1)
+		else
 		{
 			move(disks-1, towerA, towerC, towerB);
 			
 			move(disks-1, towerA, towerB, towerC);
 			
 			move(disks-1, towerB, towerC, towerA);
-
 		}
 	}
 	
-	public static void printMovements()
+	public static String printMovements()
 	{
+		String move = "";
 		for(int i = 0; i < towers.length; i++)
 		{
-			System.out.print(towers[i]);
+			move += towers[i]+" ";
 		}
-		System.out.print("\n");
+		move+="\n";
+		
+		return move;
 	}
 	
+	public static void importData() throws IOException
+	{
+		
+		String line = br.readLine();
+		int m = 0;
+		
+		while(line != null)
+		{
+			try
+			{ 
+				m = Integer.parseInt(line);
+			
+				for(int i = 1; i <= m; i++)
+				{
+
+					line = br.readLine();
+
+					towers[0] = Integer.parseInt(line);
+					towers[1] = 0;
+					towers[2] = 0;
+					bw.write(printMovements());
+					move(towers[0], 0, 1, 2);
+
+					bw.write("\n");
+				}
+
+			}catch(NumberFormatException ex){
+				line = br.readLine();
+			}
+			
+		}
+		br.close();
+		bw.close();
+	}
 	
 }
